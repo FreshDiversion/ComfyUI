68f65b60fa3d01a79910de5eebfc687c8e6c373f
FreshDiversion <freshdiversion@gmail.com>
2024-10-02 15:14:46 +0900

    Update comfy_provisioning_v2_basic_imggen.sh

--- comfy_provisioning_v2_basic_imggen.sh
+++ comfy_provisioning_v2_basic_imggen.sh
@@ -24,7 +24,7 @@ NODES=(
     "https://github.com/rgthree/rgthree-comfy"
     "https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes"
     "https://github.com/WASasquatch/was-node-suite-comfyui"
-    "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
+    #"https://github.com/ltdrdata/ComfyUI-Impact-Pack"
     
     "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
 
