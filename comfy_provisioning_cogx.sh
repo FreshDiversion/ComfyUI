9a5030819c0b7f717b7924e4f74321154fb5c979
FreshDiversion <freshdiversion@gmail.com>
2024-10-27 21:30:13 +0900

    Update comfy_provisioning_cogx.sh

--- comfy_provisioning_cogx.sh
+++ comfy_provisioning_cogx.sh
@@ -163,7 +163,8 @@ function provisioning_get_nodes() {
 
 function provisioning_get_default_workflow() {
     if [[ -n $DEFAULT_WORKFLOW ]]; then
-        workflow_json=$(curl -s "$DEFAULT_WORKFLOW")
+        #workflow_json=$(curl -s "$DEFAULT_WORKFLOW")
+        workflow_json=$(curl -s "https://github.com/FreshDiversion/ComfyUI/blob/main/workflows/video/cogvideox_I2V_example_01_workflow.json")
         if [[ -n $workflow_json ]]; then
             echo "export const defaultGraph = $workflow_json;" > /opt/ComfyUI/web/scripts/defaultGraph.js
         fi
