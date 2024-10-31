843426905d858aacf28655a36ddac602cb530668
FreshDiversion <freshdiversion@gmail.com>
2024-10-31 09:11:45 +0900

    Update comfy_provisioning_v2_basic_video.sh

--- comfy_provisioning_v2_basic_video.sh
+++ comfy_provisioning_v2_basic_video.sh
@@ -54,7 +54,7 @@ UNET_MODELS=(
 
 LORA_MODELS=(
     #"https://civitai.com/api/download/models/16576"
-    #https://huggingface.co/babyduck123/for_vast/tree/main/lora
+    #more obscure loras here -> https://huggingface.co/babyduck123/for_vast/tree/main/lora
     "https://huggingface.co/babyduck123/for_vast/resolve/main/lora/SD%201.5%20LCM%20LoRA.safetensors"
 )
 
